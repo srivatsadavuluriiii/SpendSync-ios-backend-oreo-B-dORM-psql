5b6b60555378b2b63980b51f5f190cf009841bf7
srivatsadavuluriiii <connect.davuluri@gmail.com>
2025-05-23 16:57:47 +0530

    Add initial iOS app structure with ContentView, app configuration, and Google Sign-In integration. Include asset catalog and project files. Remove combined log file.

---
+++ SpendSync-ios-mocha-sUI-OAuth-RBD/SpendSync-ios-mocha-sUI-OAuth-RBD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendSync-ios-mocha-sUI-OAuth-RBD
+//
+//  Created by srivatsa davuluri on 21/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendSync-ios-mocha-sUI-OAuth-RBD/SpendSync-ios-mocha-sUI-OAuth-RBD/SpendSync_ios_mocha_sUI_OAuth_RBDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSync_ios_mocha_sUI_OAuth_RBDApp.swift
+//  SpendSync-ios-mocha-sUI-OAuth-RBD
+//
+//  Created by srivatsa davuluri on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SpendSync_ios_mocha_sUI_OAuth_RBDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
